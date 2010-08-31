9f8af36a88a1143e1c425aea0e0789a0f5c07e73
Andrew Cholakian <andrew@andrewvc.com>
2010-08-31 08:49:04 -0700

    Fixed pub sub zmqmachine example to not sleep, but properly use register/deregister

--- 003_ZMQMachine/002_a_pub_sub_example.rb
+++ 003_ZMQMachine/002_a_pub_sub_example.rb
@@ -10,28 +10,38 @@ Thread.abort_on_exception = true
 ADDR = ZM::Address.new('127.0.0.1', 2200, :tcp)
 
 class PubHandler
-  def initialize(ctx,name)
-    @ctx  = ctx
+  def initialize(reactor,name)
+    @reactor  = reactor
     @name = name
     @sent_messages = 0
+    @write_queue   = []
   end
   
   def on_attach(socket)
+    @socket = socket
     socket.bind(ADDR)
   end
 
   def on_writable(socket)
-    @sent_messages += 1
-    msg = "#{@name} | #{@sent_messages}"
-    puts "P: #{msg}"
-    socket.send_message_string(msg)
-    sleep 1
+    unless @write_queue.empty?
+      message = @write_queue.shift
+      socket.send_message_string message
+    else
+      # when the messages are sent, deregister for write events to disable
+      # the "busy" loop
+      @reactor.deregister_writable(socket)
+    end
+  end
+
+  def queue_writable_message(message)
+    @write_queue << message
+    @reactor.register_writable(@socket)
   end
 end
 
 class SubHandler
-  def initialize(ctx,name)
-    @ctx  = ctx
+  def initialize(reactor,name)
+    @reactor  = reactor
     @name = name
   end
   
@@ -46,11 +56,18 @@ class SubHandler
   end
 end
 
-reactor = ZM::Reactor.new(:my_reactor).run do |ctx|
-  ctx.pub_socket(PubHandler.new(ctx, 'pub1'))
-   
-  ctx.sub_socket(SubHandler.new(ctx, 'sub1'))
-  ctx.sub_socket(SubHandler.new(ctx, 'sub2'))
+reactor = ZM::Reactor.new(:my_reactor).run do |reactor|
+  pub_handler = PubHandler.new(reactor, 'pub1')
+  reactor.pub_socket(pub_handler)
+  count = 0
+  reactor.periodical_timer(500) do #Timer delay is in MS
+    count += 1
+    pub_handler.queue_writable_message("Message #{count}")
+  end 
+
+  reactor.sub_socket(SubHandler.new(reactor, 'sub1'))
+  reactor.sub_socket(SubHandler.new(reactor, 'sub2'))
 end
 
+Thread.new { sleep 5; reactor.stop }
 reactor.join
